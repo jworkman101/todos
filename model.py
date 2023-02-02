095e2fe39e5b0c1f0cd1865ec366f6bba40d9192
jworkman101 <jworkman88@gmail.com>
2023-02-02 19:36:20 +1100

    models

--- model.py
+++ model.py
@@ -1,9 +1,17 @@
 from pydantic import BaseModel
 
-class Item(BaseModel):
-    item: str 
-    status: str
+# class Item(BaseModel):
+#     item: str 
+#     status: str
 
 class Todo(BaseModel):
     id: int
     item: str
+    
+    class Config:
+        Schema_extra = {
+            "Example": {
+            "id": 1,
+            "item": "Example schema!"
+            }
+        }  
